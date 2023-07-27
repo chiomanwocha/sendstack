3b7b57d5a1a42f989c7881b7e8b16f2ce2878218
nwochachioma <nwochachioma1998@gmail.com>
2023-07-28 00:04:20 +0100

    fix: fixed pagination width

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -12,40 +12,41 @@ const Pagination = ({ data, page, setPage }) => {
   return (
     <Box
       px={5}
-      py={{base: 5, md: 0}}
-      gap={{base: 2, md: 0}}
+      py={{ base: 5, md: 0 }}
+      gap={{ base: 2, md: 0 }}
+      mt={2}
       display="flex"
       flexDirection={{ base: "column", md: "row" }}
       justifyContent="space-between"
       alignItems={{ base: "start", md: "center" }}
-      
     >
       <Text fontSize="sm">
         Showing page {data?.data?.page} of {data?.data?.totalPages}
       </Text>
-      <Box overflow="scroll" width="100%">
-
-      <ReactPaginate
-        className="paginate"
-        previousClassName={`${data?.data?.page === 1 && "hide-cursor"}`}
-        previousLinkClassName={`${data?.data?.page === 1 && "hide-cursor"}`}
-        nextClassName={`${page + 1 === data?.data?.totalPages && "hide-cursor"}`}
-        nextLinkClassName={`${
-          page + 1 === data?.data?.totalPages && "cursor-default hidden"
-        }`}
-        breakLabel="..."
-        nextLabel="Next"
-        onPageChange={handlePageClick}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={3}
-        pageCount={data?.data?.totalPages ?? 1}
-        previousLabel="Prev"
-        containerClassName={`paginate-container ${
-          data?.pagination?.totalPages === 1 && "hide-cursor"
-        }`}
-        activeClassName="paginate-active"
-        forcePage={pageOffset}
-      />
+      <Box overflow="scroll">
+        <ReactPaginate
+          className="paginate"
+          previousClassName={`${data?.data?.page === 1 && "hide-cursor"}`}
+          previousLinkClassName={`${data?.data?.page === 1 && "hide-cursor"}`}
+          nextClassName={`${
+            page + 1 === data?.data?.totalPages && "hide-cursor"
+          }`}
+          nextLinkClassName={`${
+            page + 1 === data?.data?.totalPages && "cursor-default hidden"
+          }`}
+          breakLabel="..."
+          nextLabel="Next"
+          onPageChange={handlePageClick}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          pageCount={data?.data?.totalPages ?? 1}
+          previousLabel="Prev"
+          containerClassName={`paginate-container ${
+            data?.pagination?.totalPages === 1 && "hide-cursor"
+          }`}
+          activeClassName="paginate-active"
+          forcePage={pageOffset}
+        />
       </Box>
     </Box>
   );
